d64543bf7e70a7fdabf557e0c09d9f2875277dd7
NotClavilux <137059698+NotClavilux@users.noreply.github.com>
2024-01-18 20:11:39 +0200

    Update RobloxFpsUnlocker.bat

--- RobloxFpsUnlocker.bat
+++ RobloxFpsUnlocker.bat
@@ -5,26 +5,27 @@ set "base_path=%userprofile%\AppData\Local\Roblox\Versions"
 set "new_folder_name=ClientSettings"
 
 rem Check if RobloxPlayerBeta.exe exists
-for /f "tokens=*" %%i in ('where /r "%base_path%" RobloxPlayerBeta.exe') do (
+for /f "delims=" %%i in ('where /r "%base_path%" RobloxPlayerBeta.exe') do (
     set "roblox_folder=%%~dpi"
-    goto :create_folder
 )
 
 rem If RobloxPlayerBeta.exe doesn't exist, show a message box and exit
-echo Roblox Not Found
-echo Please make sure Roblox is installed in the expected directory (AppData\\Local\\Roblox).
-pause
-exit /b
+if not defined roblox_folder (
+    echo Roblox Not Found
+    echo Please make sure Roblox is installed in the expected directory (AppData\\Local\\Roblox).
+    pause
+    exit /b
+)
 
-:create_folder
 set "client_settings_path=%roblox_folder%%new_folder_name%"
-if not exist "%client_settings_path%\" (
-    mkdir "%client_settings_path%"
-    
-    rem Prompt user for input
-    set /p "target_fps=Enter the desired FPS cap (e.g., 999): "
-    
-    rem Write to the JSON file using user input
+md "%client_settings_path%" 2>nul
+
+rem Prompt user for input
+set /a target_fps=0
+set /p "target_fps=Enter the desired FPS cap (e.g., 999): "
+
+rem Write to the JSON file using user input
+if !target_fps! gtr 0 (
     (
         echo {
         echo   "DFIntTaskSchedulerTargetFps": !target_fps!,
